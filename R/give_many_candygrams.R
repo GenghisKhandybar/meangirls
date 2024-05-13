eb1e8d7c29b8d216492934f0f0230bdab9d7da1d
GenghisKhandybar <william.medwid@gmail.com>
2024-05-13 11:57:28 -0700

    Added give many candygrams

---
+++ R/give_many_candygrams.R
@@ -0,0 +1,27 @@
+#' Gives Multiple Candygrams
+#' @param students The a vector of student names
+#' @param number The number of candygrams each student received
+#' @param extra_messages Extra commentary for each candygram
+#'
+#' @return A vector of candygram announcements
+#'
+#' @importFrom purrr pmap
+#'
+#' @export
+
+give_many_candygrams <- function(students, counts, extra_messages = NULL){
+  if(length(students) <= 1) {
+    print(length(students))
+    stop("Must input more than 1 candygram.")
+  }
+  if(length(students) != length(counts) |
+     (!is.null(extra_messages) & length(students) != length(extra_messages))){
+    stop("Vector lengths do not match.")
+  }
+  if(!is.null(extra_messages)){
+    purrr::pmap(.l = list(students, counts, extra_messages),
+                .f = give_candygrams)
+  }
+  purrr::pmap(.l = list(students, counts),
+              .f = give_candygrams)
+}

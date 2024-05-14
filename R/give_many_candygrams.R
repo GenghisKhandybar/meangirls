6a7c205c9895623ea151d534d63a421dcc872f7b
GenghisKhandybar <william.medwid@gmail.com>
2024-05-13 18:31:48 -0700

    Switched to vector outputs

--- R/give_many_candygrams.R
+++ R/give_many_candygrams.R
@@ -1,4 +1,5 @@
 #' Gives Multiple Candygrams
+#'
 #' @param students The a vector of student names
 #' @param number The number of candygrams each student received
 #' @param extra_messages Extra commentary for each candygram
@@ -9,7 +10,11 @@
 #'
 #' @export
 
-give_many_candygrams <- function(students, counts, extra_messages = NULL){
+give_many_candygrams <- function(students, counts,
+                                 extra_messages = NULL){
+  # Checks:
+  # Length above 1
+  # Equal lengths
   if(length(students) <= 1) {
     print(length(students))
     stop("Must input more than 1 candygram.")
@@ -18,10 +23,12 @@ give_many_candygrams <- function(students, counts, extra_messages = NULL){
      (!is.null(extra_messages) & length(students) != length(extra_messages))){
     stop("Vector lengths do not match.")
   }
+
+  # If given extra_messages, use them
   if(!is.null(extra_messages)){
-    purrr::pmap(.l = list(students, counts, extra_messages),
+    purrr::pmap_chr(.l = list(students, counts, extra_messages),
                 .f = give_candygrams)
   }
-  purrr::pmap(.l = list(students, counts),
+  purrr::pmap_chr(.l = list(students, counts),
               .f = give_candygrams)
 }
